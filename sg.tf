0f42d66d42f40d1e062834f8560c603343337e2a
Shriram33 <131244637+Shriram33@users.noreply.github.com>
2023-10-29 12:35:51 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,30 +1,76 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-south-1" # The region should be same as S3 bucket region
 }
 
-resource "aws_security_group" "terfm_project" {
-    name        = "ALLOW_SSH"
+# It will fech the AMI-ID of AWS-AMI2
 
+data "aws_ami" "fetch_ami" {
+  most_recent = true
+  owners = ["amazon"]
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
-    }
-
-    egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
   }
 }
 
+# Creating the Ansible Remote machine #
+
+resource "aws_instance" "remote" {
+  ami = data.aws_ami.fetch_ami.id
+  instance_type = local.insta
+  count = 2
+  #key_name = "<Key-pair name>"
+  tags = {
+    name = "Ansible_Remote_Node.${count.index}"
+  }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    #private_key = file("./<Key-pair name>.pem")
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    on_failure = "continue"
+    inline = [ 
+      "sudo install -y python"
+     ]
+  }
+}
+
+
+# Creating the Ansible Control machine #
+
+resource "aws_instance" "control" {
+  ami = data.aws_ami.fetch_ami.id
+  instance_type = local.insta
+  #key_name = "<Key-pair name>"
+  tags = {
+    name = "Ansible_Control_Node"
+  }
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    #private_key = file("./<keypair-name>.pem")
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    on_failure = "continue"
+    inline = [ 
+      "sudo amazon-linux-extras install -y ansible2"
+     ]
+  }
+}
+
+locals {
+  insta = "t2.micro"
+}
+
+# Output value will display the IP address of remote nodes.
+output "ip_of_remote_node" {
+  value = aws_instance.remote[*].public_ip
+}
